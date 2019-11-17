0280d50987d30a9fc5179c5fec5078ef8f2d766c
sachin chinchwade <sachin.chinchwade@niceactimize.com>
2019-11-17 12:59:46 +0530

    Init Spring Boot Project

---
+++ src/main/java/com/gcm/riskwatch/RiskwatchApplication.java
@@ -0,0 +1,13 @@
+package com.gcm.riskwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcm/riskwatch/RiskwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcm.riskwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
